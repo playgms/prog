e5f1e818c94db8823ce90b89bcb01485d525b8ea
Tejas M <97739480+playgms@users.noreply.github.com>
2023-09-05 08:39:47 +0530

    Update dijkstras.c

--- dijkstras.c
+++ dijkstras.c
@@ -1,62 +1,69 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <time.h> // Include the time library for clock functions
 
-#define MAX 10
-
-int choose(int dist[],int s[],int n)
-{
-  int j=1,min=100,w;
-  for(w=1;w<=n;w++)
-   if((dist[w]<min) && (s[w]==0))
-     {
-       min=dist[w];
-       j=w;
-     }
-   return j;
+int choose(int n, int dist[10], int visited[10]) {
+    int count = 0, v = -1, min = 999;
+    while (count < n) {
+        min = 999;
+        for (int i = 0; i < n; i++) {
+            if (dist[i] < min && visited[i] == 0) {
+                min = dist[i];
+                v = i;
+            }
+        }
+        visited[v] = 1; // Mark vertex v as visited
+        count++;
+    }
+    return v;
 }
 
-void spath(int v,int cost[][MAX],int dist[],int n)
- {
-    int w,u,i,num,s[MAX];
-    for(i=1;i<=n;i++)
-     {
-        s[i]=0;
-          dist[i]=cost[v][i];
+void dijkstras(int n, int dist[10], int visited[10], int cost[10][10]) {
+    while (1) {
+        int v = choose(n, dist, visited);
+        if (v == -1) // All vertices have been visited
+            break;
+
+        for (int i = 0; i < n; i++) {
+            if (dist[i] > dist[v] + cost[v][i])
+                dist[i] = dist[v] + cost[v][i];
         }
-     s[v]=0;
-     dist[i]=999;
-     for(num=2;num<=n;num++)
-      {
-        u=choose(dist,s,n);
-        s[u]=1;
-       for(w=1;w<=n;w++)
-       if((dist[u]+cost[u][w])<dist[w] && !s[w])  
-            dist[w]=dist[u]+cost[u][w];  
-        } 
- }
+    }
+}
 
-void main()
-{
-   int i,j,cost[MAX][MAX],dist[MAX],n,v;
-   double clk;
-   clock_t starttime,endtime;
+int main() {
+    int startVertex, n, cost[10][10];
+    int dist[10];
+    int visited[10];
+    double clk;
+    clock_t starttime, endtime;
 
-   printf("\nEnter number of vertices:");
-   scanf("%d",&n);
-   printf("\nEnter the cost of adjacency matrix\n");
-  for(i=1;i<=n;i++)
-     for(j=1;j<=n;j++)
-       scanf("%d",&cost[i][j]);
-  printf("\nEnter the source vertex");
-  scanf("%d",&v);
+    printf("\nEnter number of vertices:");
+    scanf("%d", &n);
+    printf("\nEnter the cost of adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &cost[i][j]);
+    
+    printf("\nEnter the source vertex: ");
+    scanf("%d", &startVertex);
 
-  starttime=clock();
-  spath(v,cost,dist,n);
-  endtime=clock();
-  printf("\nShortest  distance\n");
-  for(i=1;i<=n;i++)
-       printf("\n%d to %d = %d",v,i,dist[i]);
-       clk=(double)(endtime-starttime)/CLOCKS_PER_SEC;
-       printf("The time taken is %f\n",clk);
+    for (int i = 0; i < n; i++) {
+        dist[i] = 999; // Initialize distances to infinity
+        visited[i] = 0; // Mark all vertices as unvisited
+    }
+    
+    dist[startVertex] = 0;
 
+    starttime = clock();
+    dijkstras(n, dist, visited, cost);
+    endtime = clock();
+    
+    printf("\nShortest distances:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d to %d = %d\n", startVertex, i, dist[i]);
+
+    clk = (double)(endtime - starttime) / CLOCKS_PER_SEC;
+    printf("The time taken is %f seconds\n", clk);
+
+    return 0;
 }
